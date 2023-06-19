78732fd1860202d7c19b8f0a743a9c059c5e818e
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-06-19 09:24:07 +0200

    Add the correction

--- kata.php
+++ kata.php
@@ -3,3 +3,11 @@
  * Faire un programme qui écrit dans un fichier qui s'appelle "mantra.txt".
  * Il devra écrire dans ce fichier "Vive PHP !! " sur une ligne 10000 fois.
  */
+
+ $handler = fopen('mantra.txt', 'w');
+
+ for ($i=0; $i < 10000; $i++) { 
+    fwrite($handler, 'Vive PHP !!' . PHP_EOL);
+ }
+
+ fclose($handler);
